03907d88b5f2f6449f544a0b2ffcfc7f83229443
jhh0089 <jhh0089@gmail.com>
2017-05-07 21:19:29 -0500

    Game 2
    All of the content that I worked on by hand and altered from tutorials,
    except for any of the 3D character models.

---
+++ Game Programming II/Game Assets/Source/SpaceWars/Public/SpaceWarsGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "SpaceWarsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASpaceWarsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ASpaceWarsGameMode(const FObjectInitializer& ObjectInitializer);
+};
+
+
+
